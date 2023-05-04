7ad637ed48ccd1c8482f96191bad186c54b07bdf
RahimliZakir <zakirer@code.edu.az>
2023-05-04 16:49:20 +0400

    Refactoring

---
+++ app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import HowToBegin from "./components/HowToBegin";
+
+const App = () => {
+  return (
+    <div className="App">
+      <HowToBegin />
+    </div>
+  );
+};
+
+export default App;
--- app/src/index.js
+++ app/src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./scss/style.scss";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
